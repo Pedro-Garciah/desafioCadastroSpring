1812137e2b93be487f1360c0a12e7eae5d95ca83
Pedro Garcia <pedro.hage78@gmail.com>
2025-04-13 18:17:34 -0300

    chore: cria estrutura inicial do projeto

---
+++ src/main/java/com/example/desafioCadastroSpring/DesafioCadastroSpringApplication.java
@@ -0,0 +1,13 @@
+package com.example.desafioCadastroSpring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafioCadastroSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesafioCadastroSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/desafioCadastroSpring/DesafioCadastroSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.desafioCadastroSpring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioCadastroSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
